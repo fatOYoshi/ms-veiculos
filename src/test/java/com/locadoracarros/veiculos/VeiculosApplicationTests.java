caf76db9fdb19f300b3b1600b2141b87f554d9e5
Joao miguel <jm559679@gmail.com>
2025-12-02 07:25:21 -0300

    primeiro commit

---
+++ src/test/java/com/locadoracarros/veiculos/VeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locadoracarros.veiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
